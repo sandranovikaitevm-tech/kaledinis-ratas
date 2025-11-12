b2bc8c967325bc80aa2e4a8397988bc606a20ed3
sandranovikaitevm-tech <sandra.novikaite.vm@gmail.com>
2025-11-12 20:03:39 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/spin', (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
